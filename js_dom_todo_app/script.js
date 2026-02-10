2212d75e83c952776b1c853e2620fb2b092c5ef4
vignesh-gitlab <vigneshshivan6456@gmail.com>
2026-02-10 15:51:42 +0530

    first commit

---
+++ js_dom_todo_app/script.js
@@ -0,0 +1,29 @@
+const $todoText = document.querySelector('#todoText');
+const $todoList = document.querySelector('#todoList');
+
+
+$todoText.addEventListener('keypress',(e)=>{
+    if(e.code === "Enter" && e.target.value !== ''){
+        let $todo = e.target.value;
+        let $todoItem = document.createElement('li');
+        let $todoId = new Date().getTime();
+        
+        $todoItem.innerHTML = `
+                <input type="checkbox" id="${$todoId}">
+                <label for="${$todoId}">${$todo}</label>
+        `;
+        
+        $todoList.appendChild($todoItem);
+        $todoText.value = '';
+    }
+});
+
+$todoList.addEventListener('click',(e)=>{
+    if(e.target.checked){
+        e.target.parentElement.classList.add('checked');
+        //e.target.parentElement.style.textDecoration = 'line-through';
+    } else {
+        e.target.parentElement.classList.remove('checked');
+        //e.target.parentElement.style.textDecoration = 'none';
+    }
+});
